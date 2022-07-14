e142a8f919be5a7c5bcb207e68f74c68f264ef03
bhimsen <bhimsen@railsfactory.org>
2022-07-14 14:51:58 +0530

    step-3

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const students = require('./data/students')
+const students = require('./data/students');
+
 const student_details = (student) => {
   const { english, science, social } = student.marks;
-  return `id: ${student.id}, name: ${student.name}, english: ${english}, science: ${science}, social: ${social}`;
+  const totalMark = student.totalMark;
+  return `id: ${student.id}, name: ${student.name}, english: ${english}, science: ${science}, social: ${social}, total: ${totalMark}`;
 };
 
 const logResult = (result) => console.log(result);
 
+const calculateTotal = (student) => {
+  student.totalMark = student.marks.english + student.marks.science + student.marks.social;
+  return student;
+};
+
 const main = () => {
-  students.map(student_details).map(logResult);
+  return students
+    .map(calculateTotal)
+    .map(student_details)
+    .map(logResult);
 };
 
 main();
